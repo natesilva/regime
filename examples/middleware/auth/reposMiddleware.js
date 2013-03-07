d6c77d2590c8ea78e4f6c11d48a89181e0dd6b03
Nate Silva <nate@desi.com>
2013-03-07 14:36:09 -0800

    touch-up

--- examples/middleware/auth/reposMiddleware.js
+++ examples/middleware/auth/reposMiddleware.js
@@ -12,8 +12,8 @@ var USERNAME = 'gituser';
 var PASSWORD = 'bigsecret';
 
 module.exports = function(req, res, next) {
-  var header=req.headers['authorization'] || '',      // get the header
-      token=header.split(/\s+/).pop() || '',          // and the encoded auth token
+  var header=req.headers.authorization || '',         // get the header
+      token=header.split(/\s+/).pop() || '',          // and encoded auth token
       auth=new Buffer(token, 'base64').toString(),    // convert from base64
       parts=auth.split(':'),                          // split on colon
       username=parts[0],
